92dcb9fb06683debe34e38ffbeb2728f595ec5c6
losadas <losadas@uninorte.edu.co>
2023-01-22 20:04:39 -0500

    cors

--- index.js
+++ index.js
@@ -1,112 +1,114 @@
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 let notes = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456'
   },
   {
     id: 2,
-    name: "Ada Lovelance",
-    number: "39-44-5323523",
+    name: 'Ada Lovelance',
+    number: '39-44-5323523'
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345'
   },
   {
     id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122",
-  },
-];
+    name: 'Mary Poppendick',
+    number: '39-23-6423122'
+  }
+]
 
-app.get("/info", (request, response) => {
-  const date = new Date(Date.now()).toUTCString();
+app.get('/info', (request, response) => {
+  const date = new Date(Date.now()).toUTCString()
   response.send(
     `<h2>Phonebook has info for ${notes.length} people</h2> <h2>${date}</h2>`
-  );
-});
+  )
+})
 
-app.get("/api/persons", (request, response) => {
-  response.json(notes);
-});
+app.get('/api/persons', (request, response) => {
+  response.json(notes)
+})
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((element) => element.id === id);
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find((element) => element.id === id)
   if (note) {
-    response.json(note);
+    response.json(note)
   } else {
-    response.status(404).send("404 Not found");
+    response.status(404).send('404 Not found')
   }
-});
+})
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((element) => element.id !== id);
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter((element) => element.id !== id)
   if (notes) {
-    response.json(notes);
+    response.json(notes)
   } else {
-    response.status(404).send("404 Not found");
+    response.status(404).send('404 Not found')
   }
-});
+})
 
 app.post(
-  "/api/persons",
+  '/api/persons',
   morgan(function (tokens, req, res) {
     morgan.token('content', (req, res) => {
-        return JSON.stringify(req.body)
+      return JSON.stringify(req.body)
     })
     return [
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
       tokens.content(req, res)
-    ].join(" ");
+    ].join(' ')
   }),
   (request, response) => {
-    const note = request.body;
-    const correct = note.name && note.number;
+    const note = request.body
+    const correct = note.name && note.number
     if (correct) {
-      const correct2 = notes.some((element) => element.name === note.name);
+      const correct2 = notes.some((element) => element.name === note.name)
       if (!correct2) {
-        const ids = notes.map((note) => note.id);
-        const maxId = Math.max(...ids);
+        const ids = notes.map((note) => note.id)
+        const maxId = Math.max(...ids)
         const newNote = {
           id: maxId + 1,
           name: note.name,
-          number: note.number,
-        };
-        notes = [...notes, newNote];
-        response.json(newNote);
+          number: note.number
+        }
+        notes = [...notes, newNote]
+        response.json(newNote)
       } else {
-        response.status(409).send("Ya existe un usuario con este nombre");
+        response.status(409).send('Ya existe un usuario con este nombre')
       }
     } else {
-      response.status(400).send("400 Bad Request");
+      response.status(400).send('400 Bad Request')
     }
   }
-);
+)
 
-//middleware para rutas inexistentes se agrega después de todas las rutas
+// middleware para rutas inexistentes se agrega después de todas las rutas
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
-const PORT = 3001;
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
